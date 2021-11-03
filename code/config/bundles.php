d2024c1866242d60e7543a5dfd07dc74e0ccc9dc
ScottA94 <94andersonsc@googlemail.com>
2021-11-03 11:58:17 +0200

    master
    	- PHP-FPM build update
    	- misc. config changes

--- code/config/bundles.php
+++ code/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
